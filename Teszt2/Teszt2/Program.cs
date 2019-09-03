a6c0a2d631a89cd38329c1a898342bd265a78123
szajberpiraty <szajberpiraty@gmail.com>
2019-09-03 20:03:33 +0200

    Érték típusok, egész és nem egész
    #3

---
+++ Teszt2/Teszt2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Érték típusok
+            int a=0;
+            int b=12;
+            int c, d=25;
+
+            float e = 13.88f;
+            double f = 36.7894654646464646465411867978;
+            decimal g = 36.7894654646464646465411867978112356448m;
+
+            
+
+            Console.WriteLine($"e:{e} f:{f} g:{g}");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
