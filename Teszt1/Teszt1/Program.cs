8dd9c0f37d1f03e94ddf872db37980cb6b5ff802
szajberpiraty <szajberpiraty@gmail.com>
2019-09-03 19:30:04 +0200

    Teszt projekt
    #2

---
+++ Teszt1/Teszt1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teszt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+
+            String[] szovegek = new string[5];
+            String[] szovegek2 = szovegek;
+            String[] szovegek3 = {"Egy","Ketto","Három" };
+
+            szovegek[0] = "TT";
+            Console.WriteLine(szovegek[0]);
+            Console.WriteLine(szovegek2[0]);
+
+            Console.ReadLine();
+
+        }
+    }
+}
